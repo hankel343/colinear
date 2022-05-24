04ad249adec2bd82688edfd14ac7ed5552a9df55
Hankel Haldin <hankel343@gmail.com>
2022-05-24 13:39:45 -0500

    intial implementation of BruteCollinearPoints.java

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,19 +1,49 @@
-public class BruteCollinearPoints {
+import java.util.Arrays;
 
+public class BruteCollinearPoints {
+    private LineSegment[] lines;
 
     public BruteCollinearPoints(Point[] points)  // finds all line segments containing 4 points
     {
-        
+        /* Exception handling */
+        if (points == null) throw new IllegalArgumentException();
+        for (int i = 0; i < points.length; i++) { //Check for null elements
+            if (points[i] == null)
+                throw new IllegalArgumentException();
+        }
+
+        Arrays.sort(points);
+        for (int i = 0; i < points.length - 1; i++) { //Check for the same points
+            if (points[i].compareTo(points[i + 1]) == 0)
+                throw new IllegalArgumentException();
+        }
+
+        /* Algorithm */
+        lines = new LineSegment[points.length / 4]; //Create line array
+        int i = 0; //Iterator for lines array
+        for (int p = 0; p < points.length; p++) { //p
+            for (int q = p + 1; q < points.length; q++) { //q
+                for (int r = q + 1; r < points.length; r++) { //r
+                    for (int s = r + 1; s < points.length; s++) { //s
+                        if (points[p].slopeTo(points[q]) == points[q].slopeTo(points[r]) &&
+                                points[q].slopeTo(points[r]) == points[r].slopeTo(points[s])) {
+                            lines[i++] = new LineSegment(points[p], points[s]);
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
     public int numberOfSegments()   // the number of line segments
     {
-
+        return lines.length;
     }
 
     public LineSegment[] segments() // the line segments
     {
-
+        return lines;
     }
 
     public static void main(String[] args) {
